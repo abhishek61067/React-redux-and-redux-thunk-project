f20b6497ed1af737d22d53b7bf0c3ef0dec8ab29
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-03-20 18:39:50 +0545

    feat: configure logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import buyCakeReducer from "./cake/cakeReducer";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: buyCakeReducer,
+  middleware: (getDefaultMiddleware) => {
+    return [logger];
+  },
 });
 
 export default store;
